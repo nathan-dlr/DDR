6a73149eaff5eb971ec53c59d60f01f88195f912
nathan-dlr <nathan.delarosa89@gmail.com>
2024-03-12 00:43:20 -0500

    Complete target_zone function

--- DDR.py
+++ DDR.py
@@ -22,14 +22,24 @@ class Arrow:
 
     def move(self):     #move arrows up the screen 
         self.y_coord -= ARROW_VEL
+        self.target_zone()
         if self.y_coord <= -1 * self.image.get_height():        #if arrow is completely off screen
             return True 
         else:
             return False
         
     def target_zone(self):  #find how close arrow is to target
-        if self.y_coord <= 9 and self.y_coord >= 21:
+        if 8 <= self.y_coord < 22:
             self.target = "PERFECT"
+        
+        elif 22 <= self.y_coord < 40:
+            self.target = "GREAT"
+
+        elif 40 <= self.y_coord < 65:
+            self.target = "GOOD"
+
+        elif self.y_coord >= 65:
+            self.target = "MISS"
 
 
     def draw(self):     #blit image on screen
@@ -37,7 +47,7 @@ class Arrow:
             
 RIGHT_ARROW = Arrow(pygame.transform.rotate(pygame.image.load(os.path.join("Assets", "uparrow.png")), -90), 883) 
 UP_ARROW = Arrow(pygame.image.load(os.path.join("Assets", "uparrow.png")), 700) #hits outline at y = 12
-DOWN_ARROW = Arrow(pygame.transform.rotate(pygame.image.load(os.path.join("Assets", "uparrow.png")), 180), 525) 
+DOWN_ARROW = Arrow(pygame.transform.rotate(pygame.image.load(os.path.join("Assets", "uparrow.png")), 180), 527) 
 LEFT_ARROW = Arrow(pygame.transform.rotate(pygame.image.load(os.path.join("Assets", "uparrow.png")), 90), 336) 
 
 BACKGROUND = SPACE = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "background.jpg")), (WIDTH, HEIGHT))
@@ -46,14 +56,16 @@ def draw_window(active_arrows):
     WINDOW.blit(BACKGROUND, (0,0))  
     for arrow in active_arrows:
         arrow.draw()
-    LEFT_ARROW.y_coord = 21
-    LEFT_ARROW.draw()
+    UP_ARROW.y_coord = 40
+    DOWN_ARROW.y_coord = 65
+    UP_ARROW.draw()
+    DOWN_ARROW.draw()
     pygame.display.update()
 
 
 
 def main():
-    active_arrows = [RIGHT_ARROW]
+    active_arrows = []
     clock = pygame.time.Clock()
     run = True
     pygame.mixer.music.load("life_burst.mp3")
